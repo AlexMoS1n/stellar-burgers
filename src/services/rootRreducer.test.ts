7c29c8922fd1207717c29fec59492adf4bef5321
AlexMos1n <alex-mos777@yandex.ru>
2024-07-20 15:45:32 +0300

    correction after the review 1.1

--- src/services/rootRreducer.test.ts
+++ src/services/rootRreducer.test.ts
@@ -2,7 +2,7 @@ import { rootReducer } from './rootReducer';
 import store from './store';
 import { expect, describe, test } from '@jest/globals';
 
-describe('Проверка правильной инициализацию rootReducer', () => {
+describe('Проверка правильной инициализации rootReducer', () => {
   test('тест, проверяющий правильную настройку и работу rootReducer', () => {
     const anotherState = rootReducer(undefined, { type: 'UNKNOWN_ACTION' });
     expect(store.getState()).toEqual(anotherState);
